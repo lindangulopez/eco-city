1fbe4f1a78b0d2442cf38dc01500fc1ce987923d
Linda Angulo Lopez <angulo.linda@gmail.com>
2021-08-19 18:24:30 +0200

    Test Events

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,46 @@
-import { getFeaturedEvents } from '../helpers/api-util';
-import EventList from '../components/events/event-list';
+import path from 'path';
+import fs from 'fs/promises';
+
+import Link from 'next/link';
 
 function HomePage(props) {
+  const { products } = props;
+
   return (
-    <div>
-      <EventList items={props.events} />
-    </div>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
   );
 }
 
-export async function getStaticProps() {
-  const featuredEvents = await getFeaturedEvents();
+export async function getStaticProps(context) {
+  console.log('(Re-)Generating...');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
 
   return {
     props: {
-      events: featuredEvents
+      products: data.products,
     },
-    revalidate: 1800
-  }
+    revalidate: 10,
+  };
 }
 
 export default HomePage;
