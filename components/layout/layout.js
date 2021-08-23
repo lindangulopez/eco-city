78a8bb648231c00a808417cc50a92aa3405406da
Linda Angulo Lopez <angulo.linda@gmail.com>
2021-08-23 11:36:09 +0200

    feedback and footer added to layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import Link from 'next/link';
 
 import MainHeader from './main-header';
 
@@ -6,7 +7,26 @@ function Layout(props) {
   return (
     <Fragment>
       <MainHeader />
+      <hr />
       <main>{props.children}</main>
+      <hr/>
+      <h2><Link href="/feedback">Feedback</Link></h2>
+      <p></p>
+      <hr />
+      <hr/>
+     <div>
+            <footer className="footer">
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         <p className ="small"> Powered by {' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo"/></p>
+        </a>
+      </footer>
+    </div>
+      <hr/>
     </Fragment>
   );
 }
