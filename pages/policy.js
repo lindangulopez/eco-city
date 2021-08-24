631e3fa07feb68bda9ce33ecd2fab593eb469681
Linda Angulo Lopez <angulo.linda@gmail.com>
2021-08-25 00:34:02 +0200

    policy

---
+++ pages/policy.js
@@ -0,0 +1,290 @@
+import Head from 'next/head'
+
+export default function OpenSource() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Mentions Légales</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+	<h1 className="title">Mentions Légales</h1>
+        <h2 >Directeur de Publication et Editeur du site</h2>
+
+
+        <h3 className="description">Informations sur l'entreprise</h3>
+	 
+	 <h4>Madame Linda Angulo Lopez:</h4>
+        <ul>
+          <dl>
+            <dt>Raison sociale :</dt>
+            <dd>LUTCHAMMA DUDOO ANGULO LOPEZ/LINDA</dd>
+            <dt>Numéro Siren :</dt>
+            <dd>511680068</dd>
+            <dt>Numéro Siret :</dt>
+            <dd>51168006800018 (siège de l'entreprise) </dd>
+	    <dt>Numéro TVA intracommunautaire :</dt>
+	    <dd>FR84511680068</dd>
+	    <dt>Greffe :</dt>
+	    <dd>RCS Melun</dd>
+	    <dt>Code NAF / APE :</dt>
+	    <dd>7022Z (conseil pour les affaires et autres conseils de gestion) </dd>
+	    <dt>Date d'immatriculation :</dt>
+	    <dd>2009-03-16</dd>
+	    <dt>Type d'entrepreneur :</dt>
+	    <dd>Fontainebleau (Seine-et-Marne) </dd>
+	    <dt>Adresse de l'entreprise</dt>
+	    <dd>40 Rue Paul Allaine, Héricy, 77850, FR</dd>
+	    <dt>Langues anglais et français</dt>
+	    <dd>ainsi que l'afrikaans et un peu d'espagnol</dd>
+          </dl>
+	</ul>
+
+	<h3>Politics Open Source et Creative Commons.</h3>
+  <ul>
+  <dt>Lien du Projet sur Github</dt>
+	<dd>https://github.com/users/lindangulopez/projects/10</dd>
+  <dd>Le projet Explore et Collaborate utilise Github pour publier et partager des informations sur le projet, en forme de Gists et Repositories public, dont open source.</dd>
+  <dd>Une fois le projet terminé, le site Web sera rendu public sur Github et partagé en tant que modèle, dans le S56 du 2021.</dd>
+	<dt>Lien du Google Group</dt>
+	<dd>https://groups.google.com/g/explore-collaborate</dd>
+  <dt>Lien de Géo-data</dt>
+  <dd>https://arcg.is/4r5yb</dd>
+  </ul>
+
+   <h3 className="description">Informations sur le site Web</h3>
+
+
+
+	<h3>Catégories:</h3>
+        <h4 className="description">Ateliers et conseil en environnement</h4>
+	<ul>
+	<dt>Ateliers</dt>
+	<dd>Invitation to Collaborate</dd>
+	<dd>Invitation à collaborer</dd>
+	<dd>Sustainability & Development</dd>
+	<dd>Développement durable et développement</dd>
+	<dd>Beyond the SDGs</dd>
+	<dd>Au-delà des ODDs</dd>
+	<dd>Data Science</dd>
+	<dd>Science des données</dd>
+	<dd>Story Map</dd>
+	<dd>Carte de l'histoire</dd>
+	<dd>Eco Web Development</dd>
+	<dd>Développement Web Éco</dd>
+	<dt>Conseil en environnement</dt>
+	<dd>Climate Action Plans</dd>
+	<dd>Plans d'Action Climate</dd>
+	</ul>
+
+
+		
+	<h3>Hérbegement et Licence</h3>
+        <p className="description">Avec Vercel et l’intégration continue du Github</p>
+	<ul>
+	<dt>Technologie</dt>
+	<dd>JamStack, Next.js</dd>
+	<dt>Nom de Team:</dt>
+	<dd>city-sustainable-development</dd>
+  <dd>https://vercel.com/city-sustainable-development</dd>
+	<dt>Nom de repository:</dt>
+	<dd>eco-city</dd>
+  <dd>https://vercel.com/city-sustainable-development/eco-city</dd>
+	<dt>Licence</dt>
+	<dd>Open Source et Creative Commons</dd>
+	</ul>
+
+	<h4>Index du Site: 2021-08-23</h4>
+  <ul>
+  <dt>λ  (Server)  server-side renders at runtime (uses getInitialProps or getServerSideProps)</dt>
+  <dt>○  (Static)  automatically rendered as static HTML (uses no initial props)</dt>
+  <dt>●  (SSG)     automatically generated as static HTML + JSON (uses getStaticProps)</dt>
+  <dt>&nbsp;&nbsp; (ISR)     incremental static regeneration (uses revalidate in getStaticProps)</dt>
+  <br></br>
+<dd>┌ ● / (ISR: 1800 Seconds) (700 ms) </dd>
+<dd>├   /_app/</dd>
+<dd>├ λ /[uid]</dd>
+<dd>├ ○ /404 </dd>
+<dd>├ ● /allFeedback </dd>
+<dd>├ λ /api/feedback </dd>
+<dd>├ λ /api/feedback/[feedbackId] </dd>
+<dd>├ ○ /blog/collaborat</dd>
+<dd>├ ○ /blog/explore </dd>
+<dd>├ ● /ENG (ISR: 1800 Seconds)</dd>
+<dd>├ ● /events (ISR: 60 Seconds) (681 ms)</dd>
+<dd>├ ● /events/[eventId] (ISR: 30 Seconds) (1230 ms)</dd>
+<dd>├   └ css/042a0bc54b287da949ee.css </dd>
+<dd>├   ├ /events/e1</dd>
+<dd>├   ├ /events/e11</dd>
+<dd>├   ├ /events/e15</dd>
+<dd>├   └ [+3 more paths]</dd>
+<dd>├ ○ /feedback </dd>
+<dd>├ ● /FR (ISR: 1800 Seconds) (656 ms)</dd>
+<dd>├ ○ /glossary </dd>
+<dd>├ ○ /last-sales </dd>
+<dd>├ ● /products/[pid]</dd>
+<dd>├   └ /products/FR</dd>
+<dd>├ ○ /products/blog/collaborer </dd>
+<dd>├ ○ /products/blog/explorer </dd>
+<dd>└ λ /user-profile </dd>
+<dd>+ First Load JS shared by all </dd>
+<dd>├ chunks/framework.895f06.js</dd>
+<dd>├ chunks/main.20da7f.js</dd>
+<dd>├ chunks/pages/_app.64239b.js</dd>
+<dd>├ chunks/webpack.ddd010.js</dd>
+<dd>└ css/782232645a15e3d323ca.css </dd>
+
+</ul>
+
+       </main>
+
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
